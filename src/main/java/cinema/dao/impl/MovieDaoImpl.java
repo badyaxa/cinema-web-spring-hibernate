2c445547ac4dd79b558c9bc63b44ce69d8b44156
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-29 17:13:34 +0300

    impl. method add in MovieDaoImpl

--- src/main/java/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/cinema/dao/impl/MovieDaoImpl.java
@@ -4,12 +4,22 @@ import java.util.Optional;
 import cinema.dao.MovieDao;
 import cinema.lib.Dao;
 import cinema.model.Movie;
+import cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(movie);
+        transaction.commit();
+        session.close();
+        return movie;
     }
 
     @Override
