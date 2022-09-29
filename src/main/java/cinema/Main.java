5b4e34b8957c5db6c09a223f157b69c2c0b7e0a7
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-29 17:17:31 +0300

    impl. main

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -2,21 +2,21 @@ package cinema;
 
 import cinema.lib.Injector;
 import cinema.model.Movie;
-import cinema.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import cinema.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie movie = new Movie();
         movie.setTitle("Spy");
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        movie.setDescription("Susan Cooper is a 40-year-old, single, "
+                + "desk-bound CIA employee who assists her partner remotely, "
+                + "field agent Bradley Fine, on a mission. "
+                + "Fine accidentally kills arms dealer Tihomir Boyanov, "
+                + "failing to learn the location of a suitcase nuke from him.");
+        Movie spy = movieService.add(movie);
+        System.out.println("movieService.get(spy.getId()) = " + movieService.get(spy.getId()));
     }
 }
--- src/main/java/cinema/model/Movie.java
+++ src/main/java/cinema/model/Movie.java
@@ -38,4 +38,13 @@ public class Movie {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public String toString() {
+        return "Movie{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", description='" + description + '\''
+                + '}';
+    }
 }
