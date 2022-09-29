44c42f640511ef18f4a6edb3e3f5aca7b5f1ea50
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-29 17:30:57 +0300

    complete implementation of all classes in the `model` package

--- src/main/java/cinema/model/Movie.java
+++ src/main/java/cinema/model/Movie.java
@@ -1,19 +1,30 @@
 package cinema.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "movies")
-public class Movie {
+public class Movie implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private String description;
+    @ManyToMany
+    private List<Actor> actors;
+
+    public Movie() {
+    }
+
+    public Movie(String title) {
+        this.title = title;
+    }
 
     public Long getId() {
         return id;
@@ -31,12 +42,29 @@ public class Movie {
         this.title = title;
     }
 
-    public String getDescription() {
-        return description;
+    public List<Actor> getActors() {
+        return actors;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    @Override
+    public Movie clone() {
+        try {
+            Movie movie = (Movie) super.clone();
+            if (movie.getActors() != null) {
+                List<Actor> actors = new ArrayList<>();
+                for (Actor actor : movie.getActors()) {
+                    actors.add(actor.clone());
+                }
+                movie.setActors(actors);
+            }
+            return movie;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't make clone of " + this, e);
+        }
     }
 
     @Override
@@ -44,7 +72,6 @@ public class Movie {
         return "Movie{"
                 + "id=" + id
                 + ", title='" + title + '\''
-                + ", description='" + description + '\''
                 + '}';
     }
 }
