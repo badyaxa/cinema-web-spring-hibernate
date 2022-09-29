3fb19076b7c9962ed062ca18a53cdde4ffec44b2
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-29 22:49:31 +0300

    create `CinemaHallDaoImpl` and `CinemaHallServiceImpl`.

---
+++ src/main/java/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,57 @@
+package cinema.dao.impl;
+
+import cinema.dao.CinemaHallDao;
+import cinema.exception.DataProcessingException;
+import cinema.lib.Dao;
+import cinema.model.CinemaHall;
+import cinema.util.HibernateUtil;
+import java.util.List;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(cinemaHall);
+            transaction.commit();
+            return cinemaHall;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert cinema hall " + cinemaHall, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<CinemaHall> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(CinemaHall.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a cinema hall by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        String hqlQuery = "FROM CinemaHall";
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<CinemaHall> query = session.createQuery(hqlQuery, CinemaHall.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all cinema halls from DB", e);
+        }
+    }
+}
