aed93a41ada3f31fb239178f12114b128ba71e78
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-29 17:16:22 +0300

    impl. MovieDaoImpl

--- src/main/java/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/cinema/dao/impl/MovieDaoImpl.java
@@ -2,6 +2,7 @@ package cinema.dao.impl;
 
 import java.util.Optional;
 import cinema.dao.MovieDao;
+import cinema.exception.DataProcessingException;
 import cinema.lib.Dao;
 import cinema.model.Movie;
 import cinema.util.HibernateUtil;
@@ -11,19 +12,40 @@ import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
+    private SessionFactory sessionFactory;
+
+    public MovieDaoImpl() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
     @Override
     public Movie add(Movie movie) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(movie);
-        transaction.commit();
-        session.close();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save movie to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return movie;
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return Optional.empty();
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(Movie.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get movie with id=" + id + " from DB", e);
+        }
     }
 }
--- src/main/java/cinema/exception/DataProcessingException.java
+++ src/main/java/cinema/exception/DataProcessingException.java
@@ -1,4 +1,8 @@
 package cinema.exception;
 
 public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
